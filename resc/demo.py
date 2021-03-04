7ba6c2195854b722edb2f3ab83fe5b00e6317b8b
DougAF <31972099+DougAF@users.noreply.github.com>
2021-03-04 11:58:50 -0600

    update demo.py, remove test_demo.py,  remove .pre-commit.yaml

--- resc/demo.py
+++ resc/demo.py
@@ -1,13 +1,1 @@
-"""
-Some math functions.
-"""
-
-
-def add(a_num, b_num):
-    """Adds two numbers."""
-    return a_num + b_num
-
-
-def sub(a_num, b_num):
-    """Subtract two numbers."""
-    return a_num - b_num
+'''Test the code here```

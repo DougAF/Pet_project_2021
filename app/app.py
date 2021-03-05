968402ff4c1cadc2084fb02ac38d6ff47d15c3b2
DougAF <31972099+DougAF@users.noreply.github.com>
2021-03-05 12:04:03 -0600

    add data route to test ssh pushes

--- app/app.py
+++ app/app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_app():
 	return  'Application is running, good work Doug!'
+
+
+@app.route('/data')
+def show_data():
+	return "JSON"
+
+if __name__ == '__main__':
+	app.run(debug=True)

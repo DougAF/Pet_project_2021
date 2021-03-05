90a31c02d4e5ddf16629f2cd2eb0742c33060636
DougAF <31972099+DougAF@users.noreply.github.com>
2021-03-05 13:18:51 -0600

    add routes to console when running app

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder= 'react-flask-app/public/')
 
 
 @app.route("/")
@@ -14,6 +14,15 @@ def show_data():
     r = requests.get("http://google.com")
     return f"Status Code: {r.status_code}"
 
+
+@app.route("/react_index")
+def render_react_template():
+    return render_template("index.html"
+)
+
+
+with app.test_request_context():
+    print(f"Available Routes: ", url_for("show_data"), url_for("render_react_template"))
 
 if __name__ == "__main__":
     app.run(debug=True)
